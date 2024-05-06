10f868b86b98055f26ce5257f362a757e9281491
SuperJSM <143000946+SuperJSM@users.noreply.github.com>
2024-05-06 21:28:22 +0800

    you can move and look places
    My single pixel game no one knows how to play has movement, 2 rather small walls, and the ability to phase through those walls. At this point the game is literally just a 1 dimensional character's 2 dimensional game.

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 const htd = "0123456789abcdefghijklmnopqrstuvwxyz"
 function dec_to_hex(dec) {
+    if (dec == 0) {
+        return "0";
+    };
     let ans = "";
     let places = [];
     while (dec != 0){
@@ -19,33 +22,72 @@ function dec_to_hex(dec) {
 };
 
 
-function hex_to_colour(red, blue, green) {
+function hex_to_colour(red, green, blue) {
     if (red.length == 1) {
-        red = "0" + red
-    };
-    if (blue.length == 1) {
-        blue = "0" + blue
+        red = "0" + red;
     };
     if (green.length == 1) {
-        green = "0" + green
+        green = "0" + green;
+    };
+    if (blue.length == 1) {
+        blue = "0" + blue;
     };
 
-    return "#" + red + blue + green
+    return "#" + red + green + blue;
 };
 
 
-let red = 0
-let blue = 0
-let green = 0
+let red = "00";
+let green = "00";
+let blue = "00";
+
+let x = 0;
+let y = 0;
+let r = 0;
+
+let objects = [[0, 1, 0, -1], [0, 5, 0, 4]];
 
 const background = document.getElementById("background");
 background.style = "background-color: #000000";
 background.addEventListener("keydown", function(event) {
-    if (event.code == "Space") {
-        keyPress();
+    if (event.code == "KeyD") {
+        x += 1;
       };
+    if (event.code == "KeyA") {
+        x -= 1;
+    };
+    if (event.code == "KeyW") {
+        y += 1;
+    };
+    if (event.code == "KeyS") {
+        y -= 1;
+    };
+    if (event.code == "ArrowLeft") {
+        r = 180;
+    };
+    if (event.code == "ArrowRight") {
+        r = 0;
+    };
+    update();
 });
 
-function keyPress() {
-    background.style.background = "#0000ff";
+function update() {
+    console.log(x, y, r);
+    blue = "00";
+    for (let i = 0; i < objects.length; i++) {
+        if (x <= 0 && r == 0) {
+            if (y <= objects[i][1] && y >= objects[i][3] && x > -255) {
+                blue = dec_to_hex(255 + x);
+            };
+            continue;
+        };
+        if (x >= 0 && r == 180) {
+            if (y <= objects[i][1] && y >= objects[i][3] && x < 255) {
+                blue = dec_to_hex(255 - x);
+            };
+        };
+    };
+
+    console.log(hex_to_colour(red, green, blue));
+    background.style = "background-color: " + hex_to_colour(red, green, blue);
 };
